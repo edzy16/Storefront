08adc44e3777bdb922a93c60c509491aba2001cf
edzy16 <edwinsiby16@gmail.com>
2022-08-13 23:26:00 +0530

    fixing the config

--- Playground/apps.py
+++ Playground/apps.py
@@ -4,11 +4,3 @@ from django.apps import AppConfig
 class playgroundConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'playground'
-
-class storeconfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'storefront'
-
-class tagsconfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tags'
