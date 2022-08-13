3fc0676f74e1b3f3f0e2ec28e31ee4fd05ca9f95
edzy16 <edwinsiby16@gmail.com>
2022-08-13 23:15:22 +0530

    setted up the config

--- Playground/apps.py
+++ Playground/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
 
 
-class PlaygroundConfig(AppConfig):
+class playgroundConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'playground'
+
+class storeconfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'storefront'
+
+class tagsconfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tags'
